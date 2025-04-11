a0d2618471c387c55f11f111eb1014d90618641a
dapsdi <artemzg029@gmail.com>
2025-04-11 21:50:17 +0300

    UI done

--- ui.py
+++ ui.py
@@ -1,77 +1,71 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, QHBoxLayout, QVBoxLayout, QFormLayout
 
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QPushButton,
+    QListWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QTextEdit,
+    QLineEdit
+    
+)
 
-import json
+class SmartNotes(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Розумні Замітки")
+        self.resize(800, 600)
 
+        #основні елементи
+        self.text_field = QTextEdit() #поле для тексту замітки
+        self.text_field.setPlaceholderText("Введіть текст замітки...")
 
+        self.notes_list = QListWidget() #список заміток
+
+        self.btn_create_note = QPushButton("Створити замітку")
+        self.btn_delete_note = QPushButton("Видалити замітку")
+        self.btn_save_note = QPushButton("Зберегти замітку")
+
+        self.tag_list = QListWidget() #список тегів
+ 
+        self.write_tag = QLineEdit() #текстове поле для тегів
+        self.write_tag.setPlaceholderText("Введіть тег...")
+        
+        #кнопки для тегів
+
+        self.btn_add_tag = QPushButton("Додати до замітки")
+        self.btn_delete_tag = QPushButton("Відкріпити з замітки")
+        self.btn_search_tag = QPushButton("Шукати за тегом")
+
+        #права частина
+        right_layout = QVBoxLayout()
+
+        right_layout.addWidget(QLabel("Список заміток"))
+        right_layout.addWidget(self.notes_list)
+
+        right_layout.addWidget(self.btn_create_note)
+        right_layout.addWidget(self.btn_save_note)
+        right_layout.addWidget(self.btn_delete_note)
+
+        right_layout.addWidget(QLabel("Список тегів"))
+        right_layout.addWidget(self.tag_list)
+
+        right_layout.addWidget(self.write_tag)
+        right_layout.addWidget(self.btn_add_tag)
+        right_layout.addWidget(self.btn_delete_note)
+        right_layout.addWidget(self.btn_search_tag)
+
+        # Головне компонування
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.text_field, 2)
+        main_layout.addLayout(right_layout, 1)
+
+        self.setLayout(main_layout)
+
+#запуск
 app = QApplication([])
-
-
-#Інтерфейс програми
-# параметри вікна програми
-notes_win = QWidget()
-notes_win.setWindowTitle('Розумні замітки')
-notes_win.resize(900, 600)
-
-
-# віджети вікна програми
-list_notes = QListWidget()
-list_notes_label = QLabel('Список заміток')
-
-
-button_note_create = QPushButton('Створити замітку') # з'являється вікно з полем "Введіть ім'я замітки"
-button_note_del = QPushButton('Видалити замітку')
-button_note_save = QPushButton('Зберегти замітку')
-
-
-field_tag = QLineEdit('')
-field_tag.setPlaceholderText('Введіть тег...')
-field_text = QTextEdit()
-button_tag_add = QPushButton('Додати до замітки')
-button_tag_del = QPushButton('Відкріпити від замітки')
-button_tag_search = QPushButton('Шукати замітки по тегу')
-list_tags = QListWidget()
-list_tags_label = QLabel('Список тегів')
-
-
-# розташування віджетів по лейаутах
-layout_notes = QHBoxLayout()
-col_1 = QVBoxLayout()
-col_1.addWidget(field_text)
-
-
-col_2 = QVBoxLayout()
-col_2.addWidget(list_notes_label)
-col_2.addWidget(list_notes)
-row_1 = QHBoxLayout()
-row_1.addWidget(button_note_create)
-row_1.addWidget(button_note_del)
-row_2 = QHBoxLayout()
-row_2.addWidget(button_note_save)
-col_2.addLayout(row_1)
-col_2.addLayout(row_2)
-
-
-col_2.addWidget(list_tags_label)
-col_2.addWidget(list_tags)
-col_2.addWidget(field_tag)
-row_3 = QHBoxLayout()
-row_3.addWidget(button_tag_add)
-row_3.addWidget(button_tag_del)
-row_4 = QHBoxLayout()
-row_4.addWidget(button_tag_search)
-
-
-col_2.addLayout(row_3)
-col_2.addLayout(row_4)
-
-
-layout_notes.addLayout(col_1, stretch = 2)
-layout_notes.addLayout(col_2, stretch = 1)
-notes_win.setLayout(layout_notes)
-
-
-# запуск програми
-notes_win.show()
+window = SmartNotes()
+window.show()
 app.exec_()
